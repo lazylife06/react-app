6feb1483c4682a3b037c75eda7ac33ed8f64b9ea
lazylife06 <jihokim234@gmail.com>
2025-07-15 12:02:59 +0900

    테스트

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const btn = screen.getByRole('button');
+  const image = screen.getByRole('img');
+
+  expect(btn).toBeInTheDocument();
+  expect(image).toBeInTheDocument();
 });
