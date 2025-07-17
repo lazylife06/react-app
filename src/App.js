cb66d8c06b96ced4cf60c700f4fc4bd14a4ad7b8
lazylife06 <jihokim234@gmail.com>
2025-07-17 11:53:40 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -2,19 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+const Shoe = ({brand, des, price}) => {
+  return (
+    <div className='product'>
+        <img src={logo} alt='pic'></img>
+        {brand}<br></br>
+        <span>{des}</span><br></br>
+        {price}원<br></br>
+        <button className='add'>담기</button>
+      </div>
+  )
+}
+
+const data = [
+  {
+    brand:"브랜드 A",
+    des:"상품 설명",
+    price:35000,
+  },
+  {
+    brand:"브랜드 B",
+    des:"상품 설명",
+    price:45000,
+  }
+]
+
 function App() {
   return (
     <div className='App'>
-      <div className='Title'></div>
-
-      <div className='product'>
-        <img src={logo} alt='pic'></img>
-        브랜드A<br></br>
-        <span>편안하고 착용감이 좋은 신발</span><br></br>
-        35,000원<br></br>
-        <button className='add'>담기</button>
+      <div className='Title'>
+        <button className='Shoppingbag'>장바구니</button>
       </div>
-      
+      <div className='Content'>
+        {data.map(pro=>
+          <Shoe key={pro.brand} brand={pro.brand} des={pro.des} price={pro.price}></Shoe>
+        )}
+      </div>
       
     </div>
   );
